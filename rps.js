5dc7511360ce03b0c3c8c76e96c2fe4c3c7496a4
Rui Moreira <ruizinho@Mac.home.local>
2025-04-21 15:44:59 +0200

    I changed around the code to make human choice case-insensitive. I also created the code to play a round in which live the rules for winning rock,paper,scissors (ie. rock beats scissors, etc). The game is functioning taking human input and matching it against computer generated input and deciding a winner.

--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice(max) {
     let x = Math.floor(Math.random()*max);
     if (x == 1){
@@ -15,21 +17,43 @@ function getComputerChoice(max) {
   //console.log(getComputerChoice(3));
 
   function getHumanChoice() {
-    let choice = prompt("Choose Rock, Paper or Scissors:")
-    finalChoice = choice.toUpperCase();
-    if (finalChoice === "ROCK" || finalChoice === "SCISSORS" || finalChoice === "PAPER"){
-    return finalChoice;
-    }
-    else{
-        return "wrong input!";
-    }
+    let choice = prompt("Choose Rock, Paper or Scissors:");
+    return choice.toUpperCase();
   
   }
-  console.log(getHumanChoice());
+  //console.log(getHumanChoice());
 
   let counterHuman = 0
   let counterComputer = 0
   let humanScore = ++counterHuman;
   let computerScore = ++counterComputer; 
-  alert(humanScore);
-  alert(computerScore);
+  //alert(humanScore);
+  //alert(computerScore);
+
+  function playRound(humanChoice, computerChoice){
+    if (humanChoice === "ROCK" && computerChoice === "SCISSORS"){
+        return "You win! Rock beats scissors!";}
+    else if (humanChoice === "ROCK" && computerChoice === "PAPER"){
+        return "You lose! Paper beats rock!";}
+    else if (humanChoice === "ROCK" && computerChoice === "ROCK"){
+        return "It's a tie!";}
+    else if (humanChoice === "SCISSORS" && computerChoice === "SCISSORS"){
+        return "It's a tie!";}
+    else if (humanChoice === "SCISSORS" && computerChoice === "PAPER"){
+        return "You win! Scissors beats paper!";}
+    else if (humanChoice === "SCISSORS" && computerChoice === "ROCK"){
+        return "You lose! Rock beats scissors!";}
+    else if (humanChoice === "PAPER" && computerChoice === "SCISSORS"){
+            return "You lose! Scissors beats paper!";}
+    else if (humanChoice === "PAPER" && computerChoice === "PAPER"){
+            return "It's a tie!";}
+    else if (humanChoice === "PAPER" && computerChoice === "ROCK"){
+            return "You win! Paper beats rock!"; 
+  }
+}
+
+   const humanSelection = getHumanChoice();
+   const computerSelection = getComputerChoice(3); 
+   console.log(playRound(humanSelection,computerSelection));
+
+  
