06cde1b61a8ab62c00d3b41cb1d5ee3f74f9aef8
Rui Moreira <ruizinho@Mac.home.local>
2025-04-21 14:21:09 +0200

    created the function to prompt human user for their choise of rock,paper or scissors. have made this prompt case-insensitive as well and have also added a check in case input is wrong.

--- rps.js
+++ rps.js
@@ -1,15 +1,28 @@
 function getComputerChoice(max) {
     let x = Math.floor(Math.random()*max);
     if (x == 1){
-        return "Rock";
+        return "ROCK";
     }
     else if (x == 2){
-        return "Paper";
+        return "PAPER";
     }
     else{
-        return "Scissors";
+        return "SCISSORS";
     }
 
   }
 
-  console.log(getComputerChoice(3));
+  //console.log(getComputerChoice(3));
+
+  function getHumanChoice() {
+    let choice = prompt("Choose Rock, Paper or Scissors:")
+    finalChoice = choice.toUpperCase();
+    if (finalChoice === "ROCK" || finalChoice === "SCISSORS" || finalChoice === "ROCK"){
+    return finalChoice;
+    }
+    else{
+        return "wrong input!";
+    }
+  
+  }
+  console.log(getHumanChoice());
